4e032cf89392c60f8a7b641bd094f0c598c01f26
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-08-06 18:47:36 -0400

    add inventory panel and inventory display control

---
+++ Assets/Scripts/InventoryDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryDisplay : MonoBehaviour
+{
+  // Start is called before the first frame update
+  public void ShowOnly(int itemType)
+  {
+    for (int i = 0; i < transform.childCount; i++)
+    {
+      InventoryItemButton thisItem = transform.GetChild(i).GetComponent<InventoryItemButton>();
+      thisItem.gameObject.SetActive(thisItem.index == itemType);
+    }
+  }
+
+  // Update is called once per frame
+  public void ShowAll()
+  {
+    for (int i = 0; i < transform.childCount; i++)
+    {
+      InventoryItemButton thisItem = transform.GetChild(i).GetComponent<InventoryItemButton>();
+      thisItem.gameObject.SetActive(true);
+    }
+  }
+}
